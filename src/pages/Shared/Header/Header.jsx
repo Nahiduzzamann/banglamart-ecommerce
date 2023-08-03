005025f3060f9cf20864d9d11a4731f93dae7a2b
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-08-03 15:36:10 +0600

    added add sesction

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
-    return (
-        <div>
-            This is Header
-        </div>
-    );
+    const [hide,setHide]=useState(false);
+    const handleCloseAdd=() => {
+        setHide(true);
+    }
+  return (
+    <div>
+      <div className={`relative h-[35px] flex justify-end items-center ${hide && 'hidden'}`} >
+      <button className="absolute text-2xl mr-4" onClick={handleCloseAdd}><AiOutlineClose /></button>
+        <img
+          className="h-[35px] w-full object-cover"
+          src="https://banglamartecommerce.com/public/uploads/all/rD8sXSsY9A88MC4VkZcnlNtSckkDnycHQcWKqGhV.png"
+        ></img>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
