dfe9eb793ad7f627f7d6ebeb30350f186b129d8b
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-08-03 16:12:53 +0600

    header login part done

--- src/main.jsx
+++ src/main.jsx
@@ -37,7 +37,7 @@ i18n.init({
 });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>
+    <div className='bg-gray-400'>
     <I18nextProvider i18n={i18n}>
         <RouterProvider router={router} />
       </I18nextProvider>
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose, AiFillPhone } from "react-icons/ai";
+import LanguageToggle from "../../../components/LanguageToggle";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
-    const [hide,setHide]=useState(false);
-    const handleCloseAdd=() => {
-        setHide(true);
-    }
+  const { t } = useTranslation();
+  const [hide, setHide] = useState(false);
+  const handleCloseAdd = () => {
+    setHide(true);
+  };
   return (
     <div>
-      <div className={`relative h-[35px] flex justify-end items-center ${hide && 'hidden'}`} >
-      <button className="absolute text-2xl mr-4" onClick={handleCloseAdd}><AiOutlineClose /></button>
+      {/* Add Section  */}
+      <div
+        className={`relative h-[35px] flex justify-end items-center ${
+          hide && "hidden"
+        }`}
+      >
+        <button className="absolute text-2xl mr-4" onClick={handleCloseAdd}>
+          <AiOutlineClose />
+        </button>
         <img
           className="h-[35px] w-full object-cover"
           src="https://banglamartecommerce.com/public/uploads/all/rD8sXSsY9A88MC4VkZcnlNtSckkDnycHQcWKqGhV.png"
         ></img>
+      </div>
+      {/* Log In Section  */}
+      <div className="bg-white border-b-2">
+        <div className="container mx-auto">
+          <div className="flex justify-between">
+            <div>
+              <LanguageToggle></LanguageToggle>
+            </div>
+            <div className="flex items-center">
+                <AiFillPhone/>
+              <p className="mr-4 hidden lg:block">{t('header.number')}</p>
+              <p className="mr-4">{t('header.login')}</p>
+              <p>{t('header.registration')}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
