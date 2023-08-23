5d3550e8a0cba27ce0ea41a823394dbd3ed39838
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-08-23 14:41:46 +0600

    ok

--- src/pages/AllSeller/AllSeller.jsx
+++ src/pages/AllSeller/AllSeller.jsx
@@ -1,26 +1,21 @@
 import { Link } from "react-router-dom";
 import "./buttonstyle.css";
-import SellerShopCart from "../../components/SellerShopCart";
+import EmptyContent from "../../components/EmptyContent";
 const AllSeller = () => {
+  const AllSellers = false;
   return (
     <div className="m-1 lg:m-0">
       <div className="container mx-auto">
         <div id="main" className="mt-4 mb-4">
-          <Link to='/seller-form' id="animatedButton"></Link>
+          <Link to="/seller-form" id="animatedButton"></Link>
         </div>
         <div className="shadow-xl shadow-BackgroundColor rounded">
-            <h1 className=" lg:mt-10 border-b border-b[1px] border-b-BorderColor text-SubTextColor">All Sellers</h1>
-            <div className="p-4 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-                <SellerShopCart></SellerShopCart>
-            </div>
+          <h1 className=" lg:mt-10 border-b border-b[1px] border-b-BorderColor text-SubTextColor">
+            All Sellers
+          </h1>
+          <div className="p-4 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
+            {AllSellers ? '':(<EmptyContent text='Currently No Seller Available'></EmptyContent>)}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/FlashSale/FlashSale.jsx
+++ src/pages/FlashSale/FlashSale.jsx
@@ -1,16 +1,29 @@
+import EmptyContent from "../../components/EmptyContent";
 import FlashSaleBanner from "../../components/FlashSaleBanner";
 
 const FlashSalePage = () => {
-    return (
-        <div className="container mx-auto">
-            <div>
-                <FlashSaleBanner></FlashSaleBanner>
-            </div>
-            <div className="mt-4">
-                <h1 className="text-SubTextColor">Hunt This Special Offer</h1>
-            </div>
+  const products = false;
+  return (
+    <div className="container mx-auto">
+      <div>
+        <FlashSaleBanner></FlashSaleBanner>
+      </div>
+      <div className="m-1 lg:m-0">
+        <div className="mt-4">
+          <h1 className="text-SubTextColor mb-4">Hunt This Special Offer</h1>
         </div>
-    );
+        <div>
+          <div>
+            {products ? (
+              <div></div>
+            ) : (
+              <EmptyContent text='Currently No Offer available!!!'></EmptyContent>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FlashSalePage;
