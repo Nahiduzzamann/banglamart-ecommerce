936a72e4697d7b9c80bec897f17439a62b1961fa
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2023-08-28 12:30:47 +0600

    adsadf

--- src/components/ScrollToTopOnRouteChange.jsx
+++ src/components/ScrollToTopOnRouteChange.jsx
@@ -1,10 +1,11 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTopOnRouteChange = () => {
   const { pathname } = useLocation();
+  
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
