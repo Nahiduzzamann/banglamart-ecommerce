9066770dd46b7c39f9bfa16b6ed024534e9f715a
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-09-02 12:09:20 +0600

    ok

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,18 +2,21 @@ import { Avatar } from "@chakra-ui/react";
 import { useState } from "react";
 import { FaUserEdit } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { Button } from "react-scroll";
 
 const Profile = () => {
   // Sample user data
   const [userData, setUserData] = useState({
     name: "John Doe",
     email: "johndoe@example.com",
+    phone: "01750666272",
     image: "",
   });
 
   // State for editable fields
   const [editedName, setEditedName] = useState(userData.name);
   const [editedEmail, setEditedEmail] = useState(userData.email);
+  const [editedPhone, setEditedPhone] = useState(userData.phone);
   const [editedImage, setEditedImage] = useState(userData.image);
   // Add more states for other user data fields as needed
 
@@ -24,6 +27,7 @@ const Profile = () => {
       ...userData,
       name: editedName,
       email: editedEmail,
+      phone: editedPhone,
       image: editedImage,
       // Update other user data fields here
     });
@@ -84,17 +88,30 @@ const Profile = () => {
             className="bg-BackgroundColor outline-BorderColor lg:w-96 p-2 rounded-md text-SubTextColor"
           />
         </div>
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.8 }}
-          className="bg-MainColor text-CardColor py-2 px-4 rounded-md mt-4 hover:bg-MainColorHover shadow-md shadow-MainColor"
-          onClick={handleUpdate}
-          disabled={
-            editedName === userData.name && editedEmail === userData.email
-          }
-        >
-          Update
-        </motion.button>
+        <div className="mt-4">
+          <label className="text-SubTextColor mr-2">Phone:</label>
+          <input
+            type="number"
+            value={editedPhone}
+            onChange={(e) => setEditedPhone(e.target.value)}
+            className="bg-BackgroundColor outline-BorderColor lg:w-96 p-2 rounded-md text-SubTextColor"
+          />
+        </div>
+        {editedName === userData.name && editedEmail === userData.email ? (
+          <button disabled className="bg-BackgroundColor text-SubTextColor py-2 px-4 rounded-md mt-4 shadow-md shadow-SubTextColor"> Update</button>
+        ) : (
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+            className="bg-MainColor text-CardColor py-2 px-4 rounded-md mt-4 hover:bg-MainColorHover shadow-md shadow-MainColor "
+            onClick={handleUpdate}
+            disabled={
+              editedName === userData.name && editedEmail === userData.email
+            }
+          >
+            Update
+          </motion.button>
+        )}
       </div>
     </div>
   );
