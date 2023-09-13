7d8bd7b9df7e8b4b9a66219cb77a74f6fcb483e0
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2023-09-14 01:25:33 +0600

    sdaf

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 //import { url } from "./authApi";
-const url = "http://localhost:1300";
+const url = "http://62.72.31.204:1300";
 const socket = io(url, {
   autoConnect: true,
   protocols: ["http", "https"],
