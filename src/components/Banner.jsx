6aa8628afc7141845d9a3a19c79fca0eab8b15e4
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-09-19 17:04:07 +0600

    ook

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -44,7 +44,7 @@ const Banner = () => {
             </div>
           ))
         ) : (
-          <div className="flex justify-center items-center p-10">
+          <div className="flex justify-center items-center p-10 ">
             <Spinner
               thickness="4px"
               speed="0.65s"
