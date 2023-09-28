efd64518bed0b6dd04364073eca8b01c4828f2fd
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-09-28 11:51:58 +0600

    ok

--- src/components/TimerFlashSell.jsx
+++ src/components/TimerFlashSell.jsx
@@ -5,9 +5,7 @@ const TimerFlashSell = ({ flashSaleData }) => {
 
   const [remainingTime, setRemainingTime] = useState(
     Math.max(0, Math.floor((endTimestamp - new Date().getTime()) / 1000))
-   
   );
-
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,44 +19,13 @@ const TimerFlashSell = ({ flashSaleData }) => {
   }, [remainingTime]);
 
   return (
-    <div className="grid grid-flow-col gap-1 lg:gap-2 text-center auto-cols-max">
-      <div className="flex flex-col justify-center items-center p-1 lg:p-3 bg-MainColor rounded-box text-neutral-content">
-        <span className="countdown font-mono text-sm lg:text-2xl">
-          <h1
-            className="text-CardColor"
-            style={{ "--value": remainingTime / (24 * 60 * 60) }}
-          ></h1>
-        </span>
-        <p className="text-CardColor">days</p>
-      </div>
-      <div className="flex flex-col justify-center items-center p-1 lg:p-3 bg-MainColor rounded-box text-neutral-content">
-        <span className="countdown font-mono text-sm lg:text-2xl">
-          <h1
-            className="text-CardColor"
-            style={{ "--value": (remainingTime / 3600) % 24 }}
-          ></h1>
-        </span>
-        <p className="text-CardColor">hours</p>
-      </div>
-      <div className="flex flex-col justify-center items-center p-1 lg:p-3 bg-MainColor rounded-box text-neutral-content">
-        <span className="countdown font-mono text-sm lg:text-2xl">
-          <h1
-            className="text-CardColor"
-            style={{ "--value": (remainingTime / 60) % 60 }}
-          ></h1>
-        </span>
-        <p className="text-CardColor">min</p>
-      </div>
-      <div className="flex flex-col justify-center items-center p-1 lg:p-3 bg-MainColor rounded-box text-neutral-content">
-        <span className="countdown font-mono text-sm lg:text-2xl">
-          <h1
-            className="text-CardColor"
-            style={{ "--value": remainingTime % 60 }}
-          ></h1>
-        </span>
-        <p className="text-CardColor">sec</p>
-      </div>
-    </div>
+    
+    <span className="countdown font-mono text-2xl text-MainColor flex items-center">
+      <span className="mt-2 mb-2 pt-1 pb-1 h-6" style={{ "--value": remainingTime / (24 * 60 * 60) }}></span>:
+      <span className="mt-2 mb-2 pt-1 pb-1 h-6" style={{ "--value": (remainingTime / 3600) % 24 }}></span>:
+      <span className="mt-2 mb-2 pt-1 pb-1 h-6" style={{ "--value": (remainingTime / 60) % 60 }}></span>:
+      <span className="mt-2 mb-2 pt-1 pb-1 h-6" style={{ "--value": remainingTime % 60 }}></span>
+    </span>
   );
 };
 
