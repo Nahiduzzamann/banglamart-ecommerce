3873c3941114c788deec69d3ef7cb6afae057bc5
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-10-08 15:42:58 +0600

    ok

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -188,18 +188,24 @@ const ProductCart = ({ product }) => {
             }}
             className="absolute flex flex-col  bg-[#fc3e3e] shadow-md shadow-[#f59090] top-0 h-24 w-24"
           >
-            <span className="ml-5 mt-[6px] text-CardColor font-semibold text-[10px]">Off</span>
+            <span className="ml-5 mt-[6px] text-CardColor font-semibold text-[10px]">
+              Off
+            </span>
             <span className="text-[12px] font-semibold ml-[10px] text-CardColor bg-[#fc3e3e]">
               {product?.offer}%
             </span>
           </div>
         ) : (
-          <div className="absolute flex items-center justify-center bg-CardColor shadow-md shadow-[#f59090] rounded-r-full top-2 p-1">
-            <span className="font-semibold text-[10px] text-[#fc3e3e] mr-1">OFF</span>
-            <span className="pl-[2px] pr-[2px] text-[14px] font-semibold text-CardColor bg-[#fc3e3e] rounded-r-full">
-              -{product?.offer}৳
-            </span>
-          </div>
+          product?.offer && (
+            <div className="absolute flex items-center justify-center bg-CardColor shadow-md shadow-[#f59090] rounded-r-full top-2 p-1">
+              <span className="font-semibold text-[10px] text-[#fc3e3e] mr-1">
+                OFF
+              </span>
+              <span className="pl-[2px] pr-[2px] text-[14px] font-semibold text-CardColor bg-[#fc3e3e] rounded-r-full">
+                -{product?.offer}৳
+              </span>
+            </div>
+          )
         )}
         {product?.freeDelivery ? (
           <div className="absolute flex items-center justify-center bg-CardColor shadow-lg rounded-l-full top-2 p-1 right-0">
