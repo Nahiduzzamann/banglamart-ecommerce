510e1f88afcc06834ba00e090a7da8b6a7ae5d0e
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-10-09 12:16:45 +0600

    ok

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -659,7 +659,7 @@ const ProductDetails = () => {
                 </div>
               </div> */}
             </div>
-            <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-2">
+            <div className="pt-4 pb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
               <motion.button
                 onClick={() => handleAddToCart(product?.id, product?.minOrder)}
                 whileHover={{ scale: 1.1 }}
