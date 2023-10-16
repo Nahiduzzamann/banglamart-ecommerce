d5e132852339a08523505641c12db71c3729dc5f
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-10-16 16:07:39 +0600

    okay

--- src/pages/TrackOrder/TrackOrder.jsx
+++ src/pages/TrackOrder/TrackOrder.jsx
@@ -12,6 +12,7 @@ import { motion } from "framer-motion";
 import { getApi } from "../../apis";
 import { TbReceiptRefund } from "react-icons/tb";
 import Swal from "sweetalert2";
+import { PiSmileySadLight } from "react-icons/pi";
 const TrackOrder = () => {
   const url = "https://api.banglamartecommerce.com.bd";
   const [loading, setLoading] = useState(false);
@@ -33,29 +34,35 @@ const TrackOrder = () => {
       });
   }, [updateOrders]);
   const handleCancelOrder = (id) => {
-    setLoading2(true);
-    getApi(`/order/cancel/${id}`, null)
-      .then((res) => {
-        setUpdateOrders(res);
-        Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: "Canceled Order",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        setLoading2(false);
-      })
-      .catch(() => {
-        Swal.fire({
-          position: "top-end",
-          icon: "error",
-          title: "Something went wrong",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        setLoading2(false);
-      });
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, cancel it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setLoading2(true);
+        getApi(`/order/cancel/${id}`, null)
+          .then((res) => {
+            setUpdateOrders(res);
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Canceled Order",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            setLoading2(false);
+          })
+          .catch(() => {
+            Swal.fire("Deleted!", "Your order has been canceled.", "success");
+            setLoading2(false);
+          });
+      }
+    });
   };
   const [deliveryState, setDeliveryState] = useState("PENDING");
 
@@ -124,7 +131,7 @@ const TrackOrder = () => {
       </Helmet>
       <div className=" p-6 bg-CardColor rounded-md shadow-md">
         <h2 className="text-2xl font-semibold text-center mb-6 text-SubTextColor">
-          Track Order
+          Track Your Order
         </h2>
         <div className="mb-4 flex flex-wrap justify-center">
           {deliveryStates.map((state) => (
@@ -146,81 +153,88 @@ const TrackOrder = () => {
           ))}
         </div>
         <div className="mb-4 ">
-          {getOrdersByState(deliveryState).map((order) => (
-            <div
-              key={order.id}
-              className="card lg:card-side bg-CardColor mb-5 shadow-xl"
-            >
-              <figure>
-                <img
-                  src={`${url}${order?.product?.thumbnail}`}
-                  className="h-60 ml-4 rounded-md"
-                />
-              </figure>
-              <div className="card-body">
-                <div className="flex items-center justify-between flex-wrap">
-                  <div>
-                    <h1 className=" text-MainColor mb-1">
-                      {order?.product?.title}
-                    </h1>
-                    <h2 className="text-SubTextColor">
-                      Quantity: {order?.quantity}
-                    </h2>
-                    <h1 className="text-SubTextColor">
-                      Total Price: {order.totalAmount}TK
-                    </h1>
-                    <div className=" text-SubTextColor mt-2">
-                      {order?.colors && (
-                        <p className="">
-                          Color:{" "}
-                          <span className="font-semibold">
-                            {order?.colors?.label}
-                          </span>{" "}
-                        </p>
-                      )}
-                      {order?.sizes && (
-                        <p>
-                          Size:{" "}
-                          <span className="font-semibold">
-                            {order?.sizes?.label}
-                            {"("}
-                            {order?.sizes?.value}
-                            {")"}
-                          </span>{" "}
-                        </p>
-                      )}
-                      {order?.specifications && (
-                        <p>
-                          Specification:{" "}
-                          <span className="font-semibold">
-                            {order?.specifications?.label}
-                          </span>{" "}
-                        </p>
-                      )}
+          {getOrdersByState(deliveryState).length > 0 ? (
+            getOrdersByState(deliveryState).map((order) => (
+              <div
+                key={order.id}
+                className="card lg:card-side bg-CardColor mb-5 shadow-xl"
+              >
+                <figure>
+                  <img
+                    src={`${url}${order?.product?.thumbnail}`}
+                    className="h-60 ml-4 rounded-md"
+                  />
+                </figure>
+                <div className="card-body">
+                  <div className="flex items-center justify-between flex-wrap">
+                    <div>
+                      <h1 className=" text-MainColor mb-1">
+                        {order?.product?.title}
+                      </h1>
+                      <h2 className="text-SubTextColor">
+                        Quantity: {order?.quantity}
+                      </h2>
+                      <h1 className="text-SubTextColor">
+                        Total Price: {order.totalAmount}TK
+                      </h1>
+                      <div className=" text-SubTextColor mt-2">
+                        {order?.colors && (
+                          <p className="">
+                            Color:{" "}
+                            <span className="font-semibold">
+                              {order?.colors?.label}
+                            </span>{" "}
+                          </p>
+                        )}
+                        {order?.sizes && (
+                          <p>
+                            Size:{" "}
+                            <span className="font-semibold">
+                              {order?.sizes?.label}
+                              {"("}
+                              {order?.sizes?.value}
+                              {")"}
+                            </span>{" "}
+                          </p>
+                        )}
+                        {order?.specifications && (
+                          <p>
+                            Specification:{" "}
+                            <span className="font-semibold">
+                              {order?.specifications?.label}
+                            </span>{" "}
+                          </p>
+                        )}
+                      </div>
                     </div>
                   </div>
+                  {deliveryState === "PENDING" && (
+                    <div className="card-actions justify-end">
+                      {loading2 ? (
+                        <div className="btn btn-info text-CardColor">
+                          <span className="loading loading-spinner loading-md"></span>
+                        </div>
+                      ) : (
+                        <motion.button
+                          whileHover={{ scale: 1.1 }}
+                          whileTap={{ scale: 0.8 }}
+                          className="btn btn-info text-CardColor"
+                          onClick={() => handleCancelOrder(order.id)}
+                        >
+                          Cancel Order
+                        </motion.button>
+                      )}
+                    </div>
+                  )}
                 </div>
-                {deliveryState === "PENDING" && (
-                  <div className="card-actions justify-end">
-                    {loading2 ? (
-                      <div className="btn btn-info text-CardColor">
-                        <span className="loading loading-spinner loading-md"></span>
-                      </div>
-                    ) : (
-                      <motion.button
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.8 }}
-                        className="btn btn-info text-CardColor"
-                        onClick={() => handleCancelOrder(order.id)}
-                      >
-                        Cancel Order
-                      </motion.button>
-                    )}
-                  </div>
-                )}
               </div>
+            ))
+          ) : (
+            <div className="flex flex-col items-center justify-center m-20">
+              <PiSmileySadLight className="text-SubTextColor text-4xl"></PiSmileySadLight>
+              <h1 className="text-SubTextColor">Empty {deliveryState.toLowerCase()} product!</h1>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
