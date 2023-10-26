eb5dab3bcdef85e10364370851e8279ff6eb71a5
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-10-26 12:05:04 +0600

    ok

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -72,7 +72,7 @@ const ProductCart = ({ product }) => {
         onMouseLeave={() => setHover(false)}
         className="w-[95%] cursor-pointer group aspect-[20/25] rounded-xl relative overflow-hidden border border-BorderColor hover:border-MainColor hover:shadow-lg"
       >
-        <div className="inset-0 absolute w-full h-full group-hover:scale-110 ease-in-out duration-300 bg-[#ffffffd7]">
+        <div className="inset-0 absolute w-full h-full group-hover:scale-110 ease-in-out duration-300 bg-CardColor">
           
           <img
             src={`${url}${product?.thumbnail}`}
