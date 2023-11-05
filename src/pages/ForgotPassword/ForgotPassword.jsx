a85c91aea8296897cfb09ae5ec8d337d8cdd072a
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-11-05 11:37:43 +0600

    reset password done

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { postApi } from "../../apis";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 const ForgotPassword = () => {
+  const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -18,30 +20,48 @@ const ForgotPassword = () => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData.email);
-    setIsLoading(true);
-    postApi(
-      "/auth/resetEmail",
-      {
-        email: formData.email,
-      },
-      null
-    )
-      .then((r) => {
-        console.log(r);
-        setIsLoading(false);
-        Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: r.data.message,
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        setErrorMessage(error?.response?.data?.message);
-      });
+
+    Swal.fire({
+      title: "Please Check Your Email",
+      text: formData.email,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Reset Password",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setIsLoading(true);
+        postApi(
+          "/auth/resetEmail",
+          {
+            email: formData.email,
+          },
+          null
+        )
+          .then((r) => {
+            setIsLoading(false);
+
+            Swal.fire({
+              title: r.data.message,
+              text: "Please Check Your Email and Rest Your Password",
+              icon: "success",
+              // showCancelButton: true,
+              confirmButtonColor: "#3085d6",
+              // cancelButtonColor: '#d33',
+              confirmButtonText: "OK",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                navigate("/");
+              }
+            });
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            setErrorMessage(error?.response?.data?.message);
+          });
+      }
+    });
   };
   return (
     <div className="w-full max-w-md p-6 bg-BackgroundColor rounded-md shadow-lg m-4 ld:m-0 mx-auto">
