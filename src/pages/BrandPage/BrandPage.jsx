eb2e9312dfec4decfa547b6d6061dafd1f3c3321
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-11-11 12:35:09 +0600

    ok

--- src/pages/BrandPage/BrandPage.jsx
+++ src/pages/BrandPage/BrandPage.jsx
@@ -2,7 +2,9 @@ import { Helmet } from "react-helmet";
 import BrandCart from "../../components/BrandCart";
 import EmptyContent from "../../components/EmptyContent";
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 const BrandPage = () => {
+  const {t}=useTranslation()
   const brands = useSelector((state) => state.brand.brand.data);
  
   return (
@@ -12,7 +14,7 @@ const BrandPage = () => {
       </Helmet>
 
       <div className="bg-CardColor p-4 lg:p-10 mt-4">
-        <h1 className=" mb-4 text-SubTextColor">Chose Your Favorite Brand</h1>
+        <h1 className=" mb-4 text-SubTextColor">{t("choseBrand")}</h1>
         <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-2">
           {brands?.length>0? (
             brands?.map((data, i) => (
