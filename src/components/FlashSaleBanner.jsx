779f0dee92865c26dc2be7dd62737f57e82ce7ab
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-11-21 11:36:50 +0600

    ok

--- src/components/FlashSaleBanner.jsx
+++ src/components/FlashSaleBanner.jsx
@@ -1,6 +1,6 @@
 
 const FlashSaleBanner = ({bannerURL}) => {
- const imgURL =bannerURL || 'https://static.vecteezy.com/system/resources/previews/017/771/015/original/flash-sale-banner-horizontal-background-of-discount-event-for-media-promotion-and-social-media-post-vector.jpg'
+ const imgURL =bannerURL || 'https://i.ibb.co/K78NBht/sell-41-01.jpg'
     return (
         <div className="container mx-auto mt-4 p-1 lg:p-0">
             <img className="max-h-[290px] w-full object-fill "   src={imgURL} alt="" />
